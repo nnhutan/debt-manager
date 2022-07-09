8f7869441aa296cbfd0a9adb7cd01de6d6a7599b
Dinh Nhu Tan <nhutan2001@gmail.com>
2022-07-09 23:31:49 +0700

    add welcome page and improve ui, fix bugs

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'users#index'
+  root 'util#welcome'
 
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
